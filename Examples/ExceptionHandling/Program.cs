78aeb277b4a98abfdcc1df8eef3fd8a4ecb8def7
Tomas Rutkauskas <tomas@baltsoft.com>
2020-07-01 13:19:15 +0300

    Create exception handling example #21

---
+++ Examples/ExceptionHandling/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using ConvertApiDotNet;
+using ConvertApiDotNet.Exceptions;
+
+namespace ExceptionHandling
+{
+    internal class Program
+    {
+        /// <summary>
+        /// The example shows how to handle a ConvertAPI exception and write details about it 
+        /// </summary>
+        public static async Task Main(string[] args)
+        {
+            try
+            {
+                var convertApi = new ConvertApi("your api secret");
+                const string sourceFile = @"..\..\..\TestFiles\test.docx";
+                
+                var convert = await convertApi.ConvertAsync("pdf", "split", 
+                    new ConvertApiFileParam(sourceFile));
+            }
+            //Catch exceptions and write details
+            catch (ConvertApiException e)
+            {
+                Console.WriteLine("Status Code: " + e.StatusCode);
+                Console.WriteLine("Response: " + e.Response);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
