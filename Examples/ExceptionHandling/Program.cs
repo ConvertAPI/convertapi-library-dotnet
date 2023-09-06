23f9fe6584243f2cb596ca3d5ec3ba55b069d22f
Tomas Rutkauskas <tomas@baltsoft.com>
2023-09-06 15:36:05 +0300

    ExceptionHandling migrated from .NET Framework to .NET

--- Examples/ExceptionHandling/Program.cs
+++ Examples/ExceptionHandling/Program.cs
@@ -1,33 +1,23 @@
-﻿using System;
-using System.Threading.Tasks;
+﻿/// <summary>
+/// The example shows how to handle a ConvertAPI exception and write details about it 
+/// </summary>
+
 using ConvertApiDotNet;
 using ConvertApiDotNet.Exceptions;
 
-namespace ExceptionHandling
+try
 {
-    internal class Program
-    {
-        /// <summary>
-        /// The example shows how to handle a ConvertAPI exception and write details about it 
-        /// </summary>
-        public static async Task Main(string[] args)
-        {
-            try
-            {
-                var convertApi = new ConvertApi("your api secret");
-                const string sourceFile = @"..\..\..\TestFiles\test.docx";
-                
-                var convert = await convertApi.ConvertAsync("pdf", "split", 
-                    new ConvertApiFileParam(sourceFile));
-            }
-            //Catch exceptions and write details
-            catch (ConvertApiException e)
-            {
-                Console.WriteLine("Status Code: " + e.StatusCode);
-                Console.WriteLine("Response: " + e.Response);
-            }
+    var convertApi = new ConvertApi("your api secret");
+    const string sourceFile = @"..\..\..\TestFiles\test.docx";
 
-            Console.ReadLine();
-        }
-    }
+    var convert = await convertApi.ConvertAsync("pdf", "split",
+        new ConvertApiFileParam(sourceFile));
 }
+//Catch exceptions and write details
+catch (ConvertApiException e)
+{
+    Console.WriteLine("Status Code: " + e.StatusCode);
+    Console.WriteLine("Response: " + e.Response);
+}
+
+Console.ReadLine();
