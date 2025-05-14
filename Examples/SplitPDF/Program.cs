6425840a1714ccbfb654bf42116275609651e65e
Tomas Rutkauskas <tomas@baltsoft.com>
2025-05-14 15:45:52 +0300

    Add SplitPDF example project for PDF splitting by text pattern
    This commit introduces the SplitPDF project to demonstrate splitting PDF files based on specific text patterns using the ConvertAPI library. It includes a sample program, project configuration, and a demo PDF file for testing purposes. The solution file has been updated to include the new project.

---
+++ Examples/SplitPDF/Program.cs
@@ -0,0 +1,30 @@
+﻿using ConvertApiDotNet;
+using ConvertApiDotNet.Exceptions;
+
+var randomFolderName = Path.GetRandomFileName();
+var outputDirectory = Path.Combine(Path.GetTempPath(), randomFolderName);
+Directory.CreateDirectory(outputDirectory);
+var demoPdf = Path.Combine(Environment.CurrentDirectory, "Files", "text-pattern-demo.pdf");
+
+
+try
+{
+    var convertApi = new ConvertApi("api_token");
+    var convert = await convertApi.ConvertAsync("pdf", "split",
+        new ConvertApiFileParam("File", demoPdf),
+        new ConvertApiParam("SplitByTextPattern", @"Chapter\s+\d+")
+    );
+    await convert.SaveFilesAsync(outputDirectory);
+}
+catch (ConvertApiException e)
+{
+    Console.WriteLine("Status Code: " + e.StatusCode);
+    Console.WriteLine("Response: " + e.Response);
+}
+
+var files = Directory.GetFiles(outputDirectory);
+Directory.Delete(outputDirectory, true);
+foreach (var file in files)
+{
+    Console.WriteLine(file);
+}
